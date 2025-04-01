ecef23120dc81ba3f82e07d26b0c83cfc7af29a7
sophiabtran <150560518+sophiabtran@users.noreply.github.com>
2025-04-01 11:29:38 -0700

    added application

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -6,27 +6,35 @@ const Apply = () => {
     <div className="pt-12 m-8">
       <div className="text-black/80 text-left py-4">
         <h2 className="text-4xl font-bold text-[#87b676]">Applications</h2>
-        <h3 className="text-xl mt-4">
+        {/* <h3 className="text-xl mt-4">
         We generally begin our application process during fall quarter, so be sure to stay updated on our timeline and announcements!
           <br></br>To learn more about Zotbins, follow us on our social media platforms for the latest news, updates, and progress on our initiatives.
+        </h3> */}
+        <h3 className="text-xl mt-4">
+          We are currently recruiting for our Web & Mobile team! Apply by April 4, 2025 @11:59 PM PST to join our team and make an impact on campus sustainability!
+           <button className="bg-[#87b676]/80 text-white hover:bg-transparent white font-semibold hover:text-[#87b676] py-2 px-4 border border-[#87b676] rounded">
+          <Link href={"http://tinyurl.com/zotbins25springdev"} target="_blank">
+            <p>Apply Here!</p>
+          </Link>
+        </button>
         </h3>
       </div>
       <div className="space-x-4">
-      <button className="bg-transparent hover:bg-[#87b676]/80 text-[#87b676] font-semibold hover:text-white py-2 px-4 border border-[#87b676] hover:border-transparent rounded">
-        <Link href={"https://www.instagram.com/zotbins/"} target="_blank">
-          <p>Instagram</p>
-        </Link>
-      </button>
-      <button className="bg-transparent hover:bg-[#87b676]/80 text-[#87b676] font-semibold hover:text-white py-2 px-4 border border-[#87b676] hover:border-transparent rounded">
-        <Link href={"https://www.linkedin.com/company/zotbins/"} target="_blank">
-          <p>LinkedIn</p>
-        </Link>
-      </button>
-      <button className="bg-transparent hover:bg-[#87b676]/80 text-[#87b676] font-semibold hover:text-white py-2 px-4 border border-[#87b676] hover:border-transparent rounded">
-        <Link href={"mailto:zotbinsuci@gmail.com"} target="_blank">
-          <p>Email</p>
-        </Link>
-      </button>
+        <button className="bg-transparent hover:bg-[#87b676]/80 text-[#87b676] font-semibold hover:text-white py-2 px-4 border border-[#87b676] hover:border-transparent rounded">
+          <Link href={"https://www.instagram.com/zotbins/"} target="_blank">
+            <p>Instagram</p>
+          </Link>
+        </button>
+        <button className="bg-transparent hover:bg-[#87b676]/80 text-[#87b676] font-semibold hover:text-white py-2 px-4 border border-[#87b676] hover:border-transparent rounded">
+          <Link href={"https://www.linkedin.com/company/zotbins/"} target="_blank">
+            <p>LinkedIn</p>
+          </Link>
+        </button>
+        <button className="bg-transparent hover:bg-[#87b676]/80 text-[#87b676] font-semibold hover:text-white py-2 px-4 border border-[#87b676] hover:border-transparent rounded">
+          <Link href={"mailto:zotbinsuci@gmail.com"} target="_blank">
+            <p>Email</p>
+          </Link>
+        </button>
       </div>
     </div>
   );
